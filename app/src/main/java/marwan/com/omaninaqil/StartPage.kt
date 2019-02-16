e87a3b340ac78abb051145991b856b563b33a2dc
Marwan <bus.marwan@gmail.com>
2019-02-16 22:12:42 +0400

    v0.1.01

---
+++ app/src/main/java/marwan/com/omaninaqil/StartPage.kt
@@ -0,0 +1,12 @@
+package marwan.com.omaninaqil
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_page)
+    }
+}
